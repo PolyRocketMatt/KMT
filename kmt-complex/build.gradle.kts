026e74a62d655c3910d7ebf32a581f0c575769ab
Matthias Kovacic <rocketpencil.studios@gmail.com>
2022-10-24 18:13:30 +0200

    Add kmt-complex

---
+++ kmt-complex/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    kotlin("jvm")
+    `java-library`
+}
+
+description = "Module for working with complex numbers"
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(project(":kmt-common"))
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/polyrocketmatt/KMT")
+            credentials {
+                username = "PolyRocketMatt"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.polyrocketmatt"
+            artifactId = "kmt-linalg"
+            version = findProperty("kmt.version") as String? ?: "0.0.1"
+
+            from(components["java"])
+
+            pom {
+                name.set("KMT Complex")
+                description.set("Module for working with complex numbers")
+                url.set("https://github.com/PolyRocketMatt/KMT")
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License v3.0")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("PolyRocketMatt")
+                        name.set("Matthias Kovacic")
+                        email.set("matthias.kovacic@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
