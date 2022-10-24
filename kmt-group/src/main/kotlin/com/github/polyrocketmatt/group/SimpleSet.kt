c25f4dfaa6fc19af10ec423d910187bb84555ea6
Matthias Kovacic <rocketpencil.studios@gmail.com>
2022-10-24 18:43:02 +0200

    Add defined sets

--- kmt-group/src/main/kotlin/com/github/polyrocketmatt/group/SimpleSet.kt
+++ kmt-group/src/main/kotlin/com/github/polyrocketmatt/group/SimpleSet.kt
@@ -12,21 +12,25 @@ import com.github.polyrocketmatt.kmt.common.annotation.Ref
  * @param elements The objects contained within to the set.
  */
 @Ref("https://en.wikipedia.org/wiki/Set_(mathematics)")
-class SimpleSet<T>(private val elements: Set<T>) {
+open class SimpleSet<T>(val elements: Set<T>) {
+
+    constructor() : this(emptySet())
+    constructor(vararg elements: T) : this(elements.toSet())
+    constructor(elements: Collection<T>) : this(elements.toSet())
 
     /**
      * Check if the set is empty.
      *
      * @return True if the set is empty, false otherwise.
      */
-    fun isEmpty(): Boolean = elements.isEmpty()
+    open fun isEmpty(): Boolean = elements.isEmpty()
 
     /**
      * Check if the set is a singleton.
      *
      * @return True if the set contains one element, false otherwise.
      */
-    fun isSingleton(): Boolean = elements.size == 1
+    open fun isSingleton(): Boolean = elements.size == 1
 
     /**
      * Check if an element is a member of the set.
@@ -34,7 +38,7 @@ class SimpleSet<T>(private val elements: Set<T>) {
      * @param element The element to check.
      * @return True if the element is a member of the set, false otherwise.
      */
-    fun contains(element: T): Boolean = element in elements
+    open fun contains(element: T): Boolean = element in elements
 
     /**
      * Check if the set is a subset of another set.
@@ -42,6 +46,6 @@ class SimpleSet<T>(private val elements: Set<T>) {
      * @param set The set to check against.
      * @return True if the set is a subset of the other set, false otherwise.
      */
-    fun isSubSet(set: SimpleSet<T>): Boolean = elements.containsAll(set.elements)
+    open fun isSubSet(set: SimpleSet<T>): Boolean = elements.containsAll(set.elements)
 
 }
